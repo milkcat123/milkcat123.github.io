46b19d6eb9c881ac11fd37183f6f72068da25020
Yuki Hiew <123milkcat@gmail.com>
2021-11-28 15:26:40 +0800

    第一次上傳

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.img-container').slick({
+        dots: true,
+        infinite: true,
+        autoplay:true,
+        lazyLoad: 'ondemand'
+            
+    });
+    $('.web-design').slick({
+        dots: true,
+        infinite: true,
+        autoplay:true
+            
+    });
+
+});
+ 
